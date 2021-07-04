338a24d4099e7d1d5d41c138cea6dffd1d482123
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-07-04 17:21:30 +0530

    This is the python code to create an annoying alarm clock

---
+++ annoying alarm.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+import time
+n=1
+c,b,a=input("Enter Date: ").split("/")
+hr,minn, sec=input("Enter the Time = ").split(":")
+shedule_date = datetime.date(int(a),int(b), int(c)
+n=1
+while n>0:
+    if time.localtime().tm_hour= int(hr) and
+        os.startfile("C:\\Users\\AmitPatnaik\\beep.mp3) //file in my local pc
+        break
+     else:
+        n+=1
